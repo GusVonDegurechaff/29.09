d9864beaeaecc39f7f7f4c348069c5828c8b5fcf
GusVonDegurechaff <115042946+GusVonDegurechaff@users.noreply.github.com>
2023-09-27 20:17:36 +0300

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 1:
+        Console.Write("Январь");
+        break;
+    case 2:
+        Console.Write("Февраль");
+        break;
+    case 3:
+        Console.Write("Март");
+        break;
+    case 4:
+        Console.Write("Апрель");
+        break;
+    case 5:
+        Console.Write("Май");
+        break;
+    case 6:
+        Console.Write("Июнь");
+        break;
+    case 7:
+        Console.Write("Июль");
+        break;
+    case 8:
+        Console.Write("Август");
+        break;
+    case 9:
+        Console.Write("Сентябрь");
+        break;
+    case 10:
+        Console.Write("Октябрь");
+        break;
+    case 11:
+        Console.Write("Ноябрь");
+        break;
+    case 12:
+        Console.Write("Декабрь");
+        break;
+}
